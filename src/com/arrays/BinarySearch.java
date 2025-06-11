e1660828df46ed4853d162253bff0fad9192ef8e
KasuKamalReddy <kasukamalreddy@gmail.com>
2025-06-11 19:45:49 +0530

    x changed

--- src/com/arrays/BinarySearch.java
+++ src/com/arrays/BinarySearch.java
@@ -26,7 +26,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch();
         int array[] = { 3, 4, 5, 6, 7, 8, 9 };
         int n = array.length;
-        int x = 4;
+        int x = 6;
         int result = ob.binarySearch(array, x, 0, n - 1);
         if (result == -1)
             System.out.println("Not found");
